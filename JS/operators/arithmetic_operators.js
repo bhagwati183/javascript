27beccf67066aac01c5c90335451dd9bbbc89bbc
bhagwati183 <bhagwatinaik18.bn@gmail.com>
2020-01-08 12:17:08 +0530

    operators

---
+++ JS/operators/arithmetic_operators.js
@@ -0,0 +1,25 @@
+var a=10,b=20;
+console.log(`addition of a+b =`,a+b);
+console.log(`substraction of a-b =`,a-b);
+console.log(`multiplication of a*b =`,a*b);
+console.log(`division of b/a =`,b/a);
+console.log(`modulus of b%a =`,b%a);
+
+// Post Increment:-After assigning the value to the variable, the value is incremented..
+var i=3;
+console.log(i++);// here i=3+1 
+console.log(i);//4
+
+// another example of post increment.
+var j=5;
+j++;//5+1=6
+console.log(j);// therefore j=6.
+
+//Pre-increment:-Before assigning the value to the variable, the value is incremented by 1.
+var k=15;
+console.log(++k);//here 1+15
+
+//OR
+var k=16;
+++k;
+console.log(k);
